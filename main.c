4331e86e16fd67d9079e3ed2f96952045ffdb691
anish2454 <shenoy.anish.227@gmail.com>
2017-11-16 19:04:34 -0500

    Fixed Fork Stuff

--- main.c
+++ main.c
@@ -8,23 +8,37 @@ int main() {
   //parent process
   int f;
   f = fork();
-  if (f == 0) {
-    int p = getpid(); 
-    printf("child pid: %d\n", p); 
-    srand( time(NULL) ); 
-    int rand_int = (rand()%15) + 5; 
-    sleep(rand_int);
-    printf("child %d is done\n", p); 
-    return rand_int;
+  if (f) {
+    printf("I am a parent\n");
+    int a;
+    wait(&a);
+    printf("child %d was asleep for %d seconds\n", f, WEXITSTATUS(a));
+    f = fork();
+    if(f){
+      int b;
+      wait(&b);
+      printf("child %d was asleep for %d seconds\n", f, WEXITSTATUS(b));
+      printf("parent %d is done\n", getpid());
+      exit(1);
+    }
+    else{
+      srand( time(NULL) );
+      int p = getpid();
+      printf("child pid: %d\n", p);
+      int rand_int = (rand()%15) + 5;
+      sleep(rand_int);
+      printf("child %d is done\n", p);
+      return rand_int;
+    }
   }
   else {
-    printf("I am a parent\n"); 
-    f = fork(); 
-    int a; 
-    wait(&a);  
-    printf("child %d was asleep for %d seconds\n", f, WEXITSTATUS(a));
-    printf("parent %d is done\n", getpid());
-    exit(1);
+    srand( time(NULL) );
+    int p = getpid();
+    printf("child pid: %d\n", p);
+    int rand_int = (rand()%15) + 5;
+    sleep(rand_int);
+    printf("child %d is done\n", p);
+    return rand_int;
   }
-  return 0; 
+  return 0;
 }
